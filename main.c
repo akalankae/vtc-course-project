9705b1bafd7b208bda4bfcdc1791a59841747189
akalankae <akalankae@gmail.com>
2024-01-23 16:56:03 +1100

    refactor: add error msg for wrong CD type and change album(char) to is_album(bool)

--- main.c
+++ main.c
@@ -5,18 +5,22 @@
  * Last modified: <date time>
  * Description: A program that a record shop might use to keep track of its inventory of CDs.
  ***************************************************************************************/
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #define MAX_TITLE 32
 #define MAX_ARTIST_NAME 64
+#define ALBUM 'a'
+#define SINGLE 's'
 
 int main(int argc, char *argv[])
 {
     char title[MAX_TITLE + 1];    // name of CD
     char artist[MAX_ARTIST_NAME]; // name of the artist
     int ntracks;                  // number of tracs
-    char album;                   // is it an ALBUM? or is it a SINGLE?
+    bool is_album;                // is it an ALBUM? or is it a SINGLE?
+    char cd_type;                 // 'a' for album, 's' for single
     double price;                 // price in dollars
 
     // CD title
@@ -28,12 +32,20 @@ int main(int argc, char *argv[])
     // Number of tracks
     printf("Enter number of tracks: ");
     scanf("%d%*c", &ntracks);
+
     // ALBUM or SINGLE?
     do
     {
-        printf("Is it an album (y/n)? ");
-        scanf("%c%*c", &album);
-    } while (album != 'y' && album != 'Y' && album != 'n' && album != 'N');
+        printf("What's the type of the CD? ('a' for album, 's' for single)\n> ");
+        scanf("%c%*c", &cd_type);
+        if (cd_type != ALBUM && cd_type != SINGLE)
+        {
+            fprintf(stderr, "Error: Please enter \"%c\" or \"%c\" only!\n", ALBUM, SINGLE);
+        }
+
+    } while (cd_type != ALBUM && cd_type != SINGLE); // do-while
+
+    is_album = cd_type == ALBUM;
 
     // price
     printf("Enter price of the CD: $");
@@ -43,7 +55,7 @@ int main(int argc, char *argv[])
     printf("     Title: %s\n", title);
     printf("     Artist: %s\n", artist);
     printf("     Number of tracks: %d\n", ntracks);
-    printf("     Type: %s\n", album == 'y' || album == 'Y' ? "Album" : "Single");
+    printf("     Type: %s\n", is_album ? "Album" : "Single");
     printf("     Retail price: %lf\n", price);
     printf("**************************************************\n");
 
