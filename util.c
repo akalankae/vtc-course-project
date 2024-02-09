8cf5c85c3e0831c8443c9da78b5cdd826fab864d
akalankae <akalankae@gmail.com>
2024-02-09 18:39:23 +1100

    feat: add clear_cd() to deallocate reserved memory used for title & artist strings of CD

--- util.c
+++ util.c
@@ -2,11 +2,15 @@
  * File name: util.c
  * Author: Akalanka Edirisinghe <akalankae@gmail.com>
  * Created on: 07 Feb 24
- * Last modified: 09 Feb 24 12.49 AM
+ * Last modified: 09 Feb 24 06.22 PM
  * Description: utility functions for main program
  * get_string() => read in a string from stdin with its NEWLINE char removed
  ***************************************************************************************/
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "database.h"
 
 extern char *get_string(char *buffer, size_t nchars)
 {
@@ -28,4 +32,15 @@ extern void trim_nl(char s[], size_t len)
                 s[i] = '\0';
         }
     }
+}
+
+/*
+ * clear_cd(cd_t *)
+ * Deallocate dynamic memory reserved for string fields of a CD structure
+ * NB: these are dynamically allocated strings pointed to by (char *) title, artist
+ */
+void clear_cd(cd_t *cd_ptr)
+{
+    free(cd_ptr->title);
+    free(cd_ptr->artist);
 }
