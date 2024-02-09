607eea9bb4018ebbe006e740df98c077625a6a84
akalankae <akalankae@gmail.com>
2024-02-09 18:29:15 +1100

    refactor: change CD struct to use char ptr for title & artist instead of fixed-length char arrays

--- database.h
+++ database.h
@@ -4,16 +4,15 @@
 /* Array size constants */
 #define TITLE_LEN 32   // maximum length of CD title
 #define ARTIST_LEN 64  // maximum length of artist name
-
 /* Data types */
 struct cd_type {
-    char title[TITLE_LEN];  // name of CD
+    char *title;  // name of CD
 #ifndef NOARTIST
-    char artist[ARTIST_LEN];  // artists name
-#endif                        /* ifdef NOARTIST */
-    int num_tracks;           // number of tracks
-    bool album;               // true = ALBUM, false = SINGLE
-    float price;              // price in dollars
+    char *artist;    // artists name
+#endif               /* ifdef NOARTIST */
+    int num_tracks;  // number of tracks
+    bool album;      // true = ALBUM, false = SINGLE
+    float price;     // price in dollars
 };
 
 typedef struct cd_type cd_t;
